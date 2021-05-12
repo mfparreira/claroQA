423f0d59697e8f86d95f57951b8ded142c00fe8d
unknown <marcelo.francoparreira@gmail.com>
2021-05-12 15:01:56 -0300

    correção do screenshot

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -31,15 +31,12 @@ end
 After do |scenario|
     add_screenshot(scenario)
 
-    if scenario.failed?
-        add_browser_logs
-    end
- end
+end
 
- def add_screenshot(scenario)
+def add_screenshot(scenario)
     nome_cenario = scenario.name.gsub(/[^A-Za-z0-9]/, '')
     nome_cenario = nome_cenario.gsub(' ','_').downcase!
     screenshot = "log/screenshots/#{nome_cenario}.png"
     page.save_screenshot(screenshot)
-    embed(screenshot, 'image/png', 'Print maroto :)')
+   
  end
